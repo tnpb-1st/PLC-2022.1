a1064cccbe00e7e946315faa0cd8bd16f416770c
Tomas Nascimento <tnpb@cin.ufpe.br>
2022-07-26 14:36:20 -0300

    added books

--- LYHGG/ch2.hs
+++ LYHGG/ch2.hs
@@ -1,8 +1,8 @@
 doubleSmallNumber x = if x > 100
-                        then x
-                        else x*2
+                then x
+                else x*2
 
-let a = [5, 4, 3, 2, 1]
+a = [5, 4, 3, 2, 1]
 
 -- head := takes a list and returns its head
 -- head a = 5
@@ -26,4 +26,18 @@ let a = [5, 4, 3, 2, 1]
 -- sum := retorna a soma dos elementos de uma lista
 -- product := retorna o produtório dos elementos de uma lista
 -- x `elem` li := retorna se o elemento x está presente na lista li
+-- ===========================================================================================================================================
+-- Ranges
+lia :: [Int]
+lia = [1, 3 .. 21] -- cria uma lista de 1 até 21 com pace 2(3-1)
+lib :: [Int]
+lib = [2, 3 .. ] -- cria uma lista infinita de 2 até INF com pace 1
+-- take N lib := mostra os N primeiros elementos da lista lib
+-- cycle liP := cycla os elementos da lista liP de forma "infinita"
+-- repeat k := cria uma lista infinita apenas de elementos com valor k
+length' xs = sum [1 | _ <- xs]
 
+-- Exercicio: pegar uma lista de triangulos cujo cada lado mede até 10 cujo perimetro é igual a 24
+
+especifiedTriangles :: [(Int,Int,Int)]
+especifiedTriangles = [(a,b,c) | a <- [1..10], b <- [1..10], c <- [1..10], a + b + c == 24, a + b > c, a + c > b, b + c > a]
