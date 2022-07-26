af4931f35fd632b68d057e4a0591f0eff2cc2fe2
Tomas Nascimento Pimentel Barros <tnpb@ug3c05.windows.cin.ufpe.br>
2022-07-26 11:45:00 -0300

    added ch2 scripts

---
+++ LYHGG/ch2.hs
@@ -0,0 +1,29 @@
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+let a = [5, 4, 3, 2, 1]
+
+-- head := takes a list and returns its head
+-- head a = 5
+
+-- tail := takes a list and returns chops its head
+-- tail a = [4,3,2,1]
+
+-- last := takes a list and returns its last element
+-- last a = 1
+
+-- init := takes a list and returns everything execpets its last element
+-- init a = [5,4,3,2,1]
+
+-- length := retorna o tamanho da lista
+-- null := retorna se a lista está vazia
+-- reverese := retorna a lista invertida
+-- take n li := extrai n elementos do início da lista li
+-- drop n li := descarta n elementos do início da lista li
+-- maximum := retorna o maior elemento da lista
+-- minimum := retorna o menor elemento da lista
+-- sum := retorna a soma dos elementos de uma lista
+-- product := retorna o produtório dos elementos de uma lista
+-- x `elem` li := retorna se o elemento x está presente na lista li
+
