fc6e6f08a2b6fd9ff57e00ab86cecfc4d66ecfab
Tomas Nascimento <tnpb@cin.ufpe.br>
2022-08-23 12:59:24 -0300

    acabei a lista de listas

--- offline_exercises/listas.hs
+++ offline_exercises/listas.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use isAsciiLower" #-}
+{-# HLINT ignore "Use foldr" #-}
 import Data.Char
 import Data.Typeable
 {- 1. Defina a função
@@ -64,3 +65,82 @@ fastFib n = fst (fibPair n)
 fibTable :: Integer -> String
 fibTable 2 = "n  fib n\n" ++ "0  0\n"
 fibTable n = fibTable(n-1) ++ show (n-2) ++ "  " ++ show (fastFib (n-2)) ++ "\n"
+
+{- 5. Defina função measure que, para um lista vazia, retorna -1 e ,para outras listas, retorna
+o tamanho da lista -}
+
+measure :: [t] -> Int
+measure [] = -1
+measure (x:xs) = 1 + measure xs
+
+{- 6. Defina a função takeFinal que retorna os últimos n elementos de uma lista dada como
+argumento -}
+
+takeFinal :: Int -> [t] -> [t]
+takeFinal _ [] = []
+takeFinal n l
+    | n >= length l = l
+    | otherwise = take n (reverse l)
+
+{- 7. Defina uma função que remove o enésimo elemento de uma lista, ou seja, retorna uma
+lista que idêntica à lista recebida como argumento, com exceção de que o enésimo não
+consta na lista de retorno. A indexação começa em zero. -}
+
+remove :: Int -> [t] -> [t]
+remove _ [] = []
+remove 0 (x:xs) = xs
+remove n (x:xs) = x : remove (n-1) xs
+
+{- 8. Dê uma definição com casamento de padrão de uma função que retorna o primeiro inteiro,
+se houver, em uma lista incrementado de um, ou retorna zero, do contrário. Apresente
+também uma solução sem casamento de padrão, mas usando funções da biblioteca. -}
+firstInt :: (Integral a) => [a] -> a
+firstInt [] = 0
+firstInt (x:xs) = x + 1
+
+firstInt' :: (Integral a) => [a] -> a
+firstInt' l = if null l then 0 else 1 + head l
+
+{- 9. Dê uma definição com casamento de padrão de uma função que faz a adição dos dois
+primeiros elementos de uma lista, se a lista contém pelo menos dois elementos; retorna
+a cabeça da lista, se houver um elemento apenas; retorna zero, do contrário. Apresente
+também uma solução sem casamento de padrão, mas usando funções da biblioteca. -}
+
+sumTwo :: (Integral a) => [a] -> a
+sumTwo [] = 0
+sumTwo [a] = a
+sumTwo (x:xs) = x + head xs
+
+sumTwo' :: (Integral a) => [a] -> a
+sumTwo' xs
+    | null xs = 0
+    | length xs == 1 = head xs
+    | otherwise = head xs + xs !! 1
+
+{- 10. Defina a função -}
+produto :: [Integer] -> Integer
+produto [] = 1
+produto (x:xs) = x * produto xs
+
+{- 11. Defina a função -}
+
+countElem :: Integer -> [Integer] -> Int
+countElem _ [] = 0
+countElem n (x:xs)
+    | n == x = 1 + countElem n xs
+    | otherwise = countElem n xs
+
+unique :: [Integer] -> [Integer]
+unique [] = []
+unique l = [x | x <- l, countElem x l == 1]
+ 
+{- 12. Defina uma função que verifica se uma lista dada como argumento está em ordem cres-
+cente. Use recursão e casamento de padrão. Não use funções de bibliotecas -}
+
+ehCrescente :: [Integer] -> Bool
+ehCrescente [] = True
+ehCrescente (x : xs)
+    | null xs = True
+    | x > head xs = False
+    | otherwise = ehCrescente xs
+
